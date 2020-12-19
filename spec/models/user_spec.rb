67cbc9e741a79c4f2c62c82d7ad4346a76d4f866
KoeusIss <sebri.issam@gmail.com>
2020-12-19 17:49:13 +0100

    Generate the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # email validation
+  it { should validate_presence_of(:email) }
+  it { should allow_value('issam@issam.com').for(:email) }
+  it { should_not allow_value('issam.issam.com').for(:email) }
+
+  # password validation
   it { should have_secure_password }
+  it { should validate_confirmation_of(:password) }
+  it { should validate_length_of(:password).is_at_least(6).on(:create) }
 end
